c960807cc7101f7e96dafef0094913889198c5fc
DH-PC\Administrator <aspwebchh@hotmail.com>
2017-11-01 23:49:55 +0800

    no message

---
+++ fast_open_work_dir/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace fast_open_work_dir
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
