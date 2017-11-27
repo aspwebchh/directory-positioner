3a49dcbe3f17eadc4feefa30db9a4d45eadbcd50
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2017-11-27 20:48:27 +0800

    交互操作优化

--- fast_open_work_dir/MainWindow.xaml.cs
+++ fast_open_work_dir/MainWindow.xaml.cs
@@ -16,58 +16,70 @@ using System.Diagnostics;
 using System.Data;
 using System.Windows.Automation;
 
-namespace fast_open_work_dir
-{
+namespace fast_open_work_dir {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+
+            ResizeMode = System.Windows.ResizeMode.NoResize;
+
             InitializeComponent();
             InitButtons();
 
-            AutomationElement aelement =  AutomationElement.RootElement
+            AutomationElement aelement = AutomationElement.RootElement
                       .FindFirst( TreeScope.Descendants, new PropertyCondition( AutomationElement.ClassNameProperty, "Shell_TrayWnd" ) )
                       .FindFirst( TreeScope.Descendants, new PropertyCondition( AutomationElement.ClassNameProperty, "ReBarWindow32" ) )
                       .FindFirst( TreeScope.Descendants, new PropertyCondition( AutomationElement.ClassNameProperty, "MSTaskSwWClass" ) )
                       .FindFirst( TreeScope.Descendants, new PropertyCondition( AutomationElement.ClassNameProperty, "MSTaskListWClass" ) )
                       .FindFirst( TreeScope.Descendants, new PropertyCondition( AutomationElement.NameProperty, "目录打开快捷工具" ) );
             if( aelement != null ) {
-                System.Windows.Rect rect = (System.Windows.Rect) aelement.GetCurrentPropertyValue( AutomationElement.BoundingRectangleProperty );
+                System.Windows.Rect rect = (System.Windows.Rect)aelement.GetCurrentPropertyValue( AutomationElement.BoundingRectangleProperty );
                 this.Left = rect.Left;
                 this.Top = rect.Top - this.Height;
             }
         }
 
-        private void InitButtons()
-        {
+        private void InitButtons() {
             var dataTable = DataSource.GetPathList();
-            dataTable.Rows.Cast<DataRow>().ToList().ForEach(item =>
-            {
-                var name = item["Name"].ToString();
-                var path = item["Path_Text"].ToString();
+            dataTable.Rows.Cast<DataRow>().ToList().ForEach( item => {
+                var name = item[ "Name" ].ToString();
+                var path = item[ "Path_Text" ].ToString();
                 var button = new Button();
                 var text = new TextBlock();
                 text.Text = name;
                 button.Content = text;
-                button.Style = Resources["ButtonNormal"] as Style;
-                button.Click += (s, e) =>
-                {
-                    System.Diagnostics.Process.Start(path);
+                button.Style = Resources[ "ButtonNormal" ] as Style;
+                button.Click += ( s, e ) => {
+                    System.Diagnostics.Process.Start( path );
                 };
-                ButtonList.Children.Add(button);
-            });
+                ButtonList.Children.Add( button );
+            } );
         }
 
-        public void ForceShow()
-        {
+        public void ForceShow() {
             this.Show();
             this.WindowState = WindowState.Normal;
             this.Visibility = System.Windows.Visibility.Visible;
             this.Activate();
         }
 
+        protected override void OnClosing( System.ComponentModel.CancelEventArgs e ) {
+            e.Cancel = true;
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void WrapPanel_MouseEnter( object sender, MouseEventArgs e ) {
+            Close.Visibility = Visibility.Visible;
+        }
+
+        private void WrapPanel_MouseLeave( object sender, MouseEventArgs e ) {
+            Close.Visibility = Visibility.Collapsed;
+        }
+
+        private void Close_Click( object sender, RoutedEventArgs e ) {
+            Application.Current.Shutdown();
+        }
     }
 }
