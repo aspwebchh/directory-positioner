2922398182b3be4527aceded6f91d9ab9578f8a6
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2018-02-04 15:52:42 +0800

    添加编辑功能和源代码项目目录结构调整

---
+++ DataSource.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Data;
+
+
+namespace fast_open_work_dir
+{
+    class DataSource
+    {
+        static DataSource()
+        {
+            CreateSourceFile();
+        }
+
+        const String SRC_FILE_NAME = "data.xml";
+
+        public static bool CreateSourceFile()
+        {
+            if (!File.Exists(SRC_FILE_NAME))
+            {
+                var fs = File.Create(SRC_FILE_NAME);
+                using (var sw = new StreamWriter(fs))
+                {
+                    var xmlString = @"<?xml version='1.0' encoding='utf-8'?>";
+                    xmlString += "<Paths></Paths>";
+                    sw.Write(xmlString);
+                    sw.Flush();
+                }
+                return true;
+            }
+            return false;
+        }
+
+
+        private static XmlElement GetElementByPath(XmlDocument doc, string path)
+        {
+            var eles = doc.GetElementsByTagName("Path");
+            var result = eles.Cast<XmlElement>().Where(item => item.InnerText == path);
+            return result.FirstOrDefault();
+        }
+
+        public static bool AddPath(string path, string name)
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(SRC_FILE_NAME);
+            if (GetElementByPath(xmlDoc, path) != null)
+            {
+                return false;
+            }
+            var newPath = xmlDoc.CreateElement("Path");
+            newPath.InnerText = path;
+            newPath.SetAttribute("Name", name);
+            var paths = xmlDoc.GetElementsByTagName("Paths")[0];
+            paths.AppendChild(newPath);
+            xmlDoc.Save(SRC_FILE_NAME);
+            return true;
+        }
+
+        public static bool EditPath( string oldPath, string path, string name ) {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load( SRC_FILE_NAME );
+            var ele = GetElementByPath( xmlDoc, oldPath );
+            if( ele  == null ) {
+                return false;
+            }
+            ele.InnerText = path;
+            ele.SetAttribute( "Name", name );
+            xmlDoc.Save( SRC_FILE_NAME );
+            return true;
+        }
+
+        public static DataTable GetPathList()
+        {
+            try
+            {
+                var dataSet = new DataSet();
+                dataSet.ReadXml(SRC_FILE_NAME);
+                return dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                return new DataTable();
+            }
+        }
+
+        public static bool Delete(string path )
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(SRC_FILE_NAME);
+            var ele = GetElementByPath(xmlDoc, path);
+            if (ele == null) return false;
+            ele.ParentNode.RemoveChild(ele);
+            xmlDoc.Save(SRC_FILE_NAME);
+            return true;
+        }
+
+        public static bool SetBgColor( string path, string color ) {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load( SRC_FILE_NAME );
+            var ele = GetElementByPath( xmlDoc, path );
+            if( ele == null ) {
+                return false;
+            }
+            ele.SetAttribute( "BgColor", color );
+            xmlDoc.Save( SRC_FILE_NAME );
+            return true;
+        }
+
+        public static bool SetTextColor( string path, string color ) {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load( SRC_FILE_NAME );
+            var ele = GetElementByPath( xmlDoc, path );
+            if( ele == null ) {
+                return false;
+            }
+            ele.SetAttribute( "TextColor", color );
+            xmlDoc.Save( SRC_FILE_NAME );
+            return true;
+        }
+    }
+}
---
+++ Edit.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fast_open_work_dir {
+    /// <summary>
+    /// Edit.xaml 的交互逻辑
+    /// </summary>
+    public partial class Edit : Window {
+        private bool isEdit = false;
+        private string oldPath = string.Empty;
+
+        public Edit() {
+            InitializeComponent();
+        }
+
+        public Edit( string name, string path ) {
+            InitializeComponent();
+
+            this.isEdit = true;
+            oldPath = path;
+            Name.Text = name;
+            Path.Text = path;
+        }
+
+        private void Button_Click( object sender, RoutedEventArgs e ) {
+            var name = Name.Text.Trim();
+            var path = Path.Text.Trim();
+            if( name == "" ) {
+                MessageBox.Show("请输入目录名称");
+                return;
+            }
+            if( path == "" ) {
+                MessageBox.Show("请输入目录路径" );
+                return;
+            }
+
+            var success = isEdit ? DataSource.EditPath(oldPath, path,name) : DataSource.AddPath( path, name );
+            if( success ) {
+                var owner = this.Owner as MainWindow;
+                owner.InitButtons();
+                this.Close();
+            } else {
+                MessageBox.Show("添加失败，请重试" );
+            }
+        }
+    }
+}
