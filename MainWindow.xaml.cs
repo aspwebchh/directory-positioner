8a325fab610d573f14edd2c769cb48a073d88045
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2018-03-21 09:10:11 +0800

    z-index 999

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace DirectoryPositioner {
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
+            this.Topmost = true;
             ResizeMode = ResizeMode.NoResize;
             InitializeComponent();
             InitPage();
